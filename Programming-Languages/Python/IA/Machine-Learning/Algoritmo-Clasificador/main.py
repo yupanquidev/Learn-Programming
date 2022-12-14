2f76964a4a10cf5c545347c6a031a2d8970d64b2
Humberto Yupanqui <npmyupanqui@gmail.com>
2022-12-14 16:24:00 -0500

    Delete algoritmo file

--- Programming-Languages/Python/IA/Machine-Learning/Algoritmo-Clasificador/main.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-import tensorflow_datasets as tfds
-
-# Descargar set de datos de entrenamiento Zalando
-datos, metadatos = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
