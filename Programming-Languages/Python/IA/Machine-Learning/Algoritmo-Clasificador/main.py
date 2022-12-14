4d360c24db9ec5345ee8f6d5dea204eba2bef53a
Humberto Yupanqui <npmyupanqui@gmail.com>
2022-12-14 16:20:28 -0500

    Move files

--- Programming-Languages/Python/IA/Machine-Learning/Algoritmo-Clasificador/main.py
+++ Programming-Languages/Python/IA/Machine-Learning/Algoritmo-Clasificador/main.py
@@ -1,14 +1,5 @@
 import tensorflow as tf
-import tensorflow_dataset as tfds
+import tensorflow_datasets as tfds
 
 # Descargar set de datos de entrenamiento Zalando
-datos, metadatos = tfds.load('fashion_nmits', )
-
-# Separar entrenamiento de pruebas
-datos_entrenamiento, datos_prueba = datos['train'], datos['tets']
-
-# Nombres de los datos
-nombres_clases = metadatos.features['label'].names
-
-# Funcion de normalizacion (0-255) -> (0-1)
-Hace que la red
+datos, metadatos = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
