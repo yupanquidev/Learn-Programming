ab11cdce444f08ceda006ff6f89f56bfe9cabd37
Humberto Yupanqui <npmyupanqui@gmail.com>
2022-12-26 10:57:25 -0500

    Christmas Tree

--- Programming-Languages/Go/Projects/ChristmasTree/main.go
+++ Programming-Languages/Go/Projects/ChristmasTree/main.go
@@ -1,7 +1,32 @@
 package main
 
-func main() {
-	for i := 0; i < count; i++ {
+import "fmt"
 
+func main() {
+	christmasTree()
+
+	for t := 0; t < 4; t++ {
+		fmt.Println("        |||||")
+	}
+}
+
+func christmasTree() {
+	n := 10
+
+	for i := 0; i < n; i++ {
+		for j := 1; j <= n-i; j++ {
+			fmt.Print(" ")
+		}
+		fmt.Print("*")
+		for k := 0; k <= i-1; k++ {
+			fmt.Print("|")
+		}
+		for j := 1; j < i; j++ {
+			fmt.Print("|")
+		}
+		if i > 0 {
+			fmt.Print("*")
+		}
+		fmt.Println()
 	}
 }
