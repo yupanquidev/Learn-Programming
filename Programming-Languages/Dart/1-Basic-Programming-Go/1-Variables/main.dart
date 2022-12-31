c60f022d98e61873bffedee57df6c73da356fa7b
Humberto Yupanqui <npmyupanqui@gmail.com>
2022-12-31 16:39:08 -0500

    Variables

--- Programming-Languages/Dart/1-Basic-Programming-Go/1-Variables/main.dart
+++ Programming-Languages/Dart/1-Basic-Programming-Go/1-Variables/main.dart
@@ -4,7 +4,7 @@ void main(List<String> args) {
   var age = 22;
 
   print("Nombre: " + name); 
-  print("Edad: " + age);
+  print(age);
 
   // Saber el tipo de variable
   print(name.runtimeType); 
