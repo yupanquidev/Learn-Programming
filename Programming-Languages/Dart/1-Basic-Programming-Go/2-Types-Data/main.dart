f7fe8169ac4730b8575a22a625149e44ca060459
Humberto Yupanqui <npmyupanqui@gmail.com>
2022-12-31 16:36:45 -0500

    Add Types Data

---
+++ Programming-Languages/Dart/1-Basic-Programming-Go/2-Types-Data/main.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  // Cadenas de texto
+  String name = "Alex";
+  var name1 = "Axel";
+
+  print("Names: " + name + name1);
+
+  // Numericos 
+  int age = 22;
+  double height = 1.70;
+
+  print(age);
+  print(height);
+
+  // Boleanos
+  bool isTeacher = false;
+  print(isTeacher);
+
+}
