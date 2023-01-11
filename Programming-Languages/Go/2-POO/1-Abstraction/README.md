143ab1f3db90cdb9d4cc54a972cc6bb651129750
Humberto Yupanqui <npmyupanqui@gmail.com>
2023-01-11 16:08:36 -0500

    add Abstraction

---
+++ Programming-Languages/Go/2-POO/1-Abstraction/README.md
@@ -0,0 +1,3 @@
+## Abstracción
+
+Los objetos solo revelan mecanismos internos que son relevantes para el uso de otros objetos, ocultando cualquier código de implementación innecesario. La clase derivada puede tener su funcionalidad extendida. Este concepto puede ayudar a los desarrolladores a realizar cambios o adiciones adicionales más fácilmente con el tiempo.
---
+++ Programming-Languages/Go/2-POO/1-Abstraction/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+// Estructura de cursos
+type Course struct {
+	Name   string
+	Price  float64
+	IsFree bool
+
+	// Estructuras compuestas
+	UserIDs []uint
+	Classes map[uint]string
+}
+
+func main() {
+
+	// Creación de instancias de objetos
+	GO := Course{
+		Name:    "Go desde cero",
+		Price:   12.22,
+		IsFree:  false,
+		UserIDs: []uint{12, 56, 89},
+		Classes: map[uint]string{
+			1: "introdución",
+			2: "Estructuras",
+			3: "maps",
+		},
+	}
+
+	// Acceder a los campos d
+	fmt.Println(GO.Name)
+}
