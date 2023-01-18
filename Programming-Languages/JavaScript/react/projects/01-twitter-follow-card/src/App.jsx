f42c75ceab5d76f640b6c304c92f4d975f016455
Humberto Yupanqui <npmyupanqui@gmail.com>
2023-01-18 17:12:08 -0500

    rename files

---
+++ Programming-Languages/JavaScript/react/projects/01-twitter-follow-card/src/App.jsx
@@ -0,0 +1,38 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+  {
+    userName: 'hyupanqui',
+    name: 'Humberto Yupanqui', 
+    isFollowing: true,
+  },
+  {
+    userName: 'renemeloquispe',
+    name: 'Pedro Zuares',
+    isFollowing: true
+  },
+  {
+    userName: 'Rene',
+    name: 'Rene Hernadez',
+    isFollowing: false
+  }
+]
+
+export function App() {
+  return (
+    <section className="App">
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
+  )
+}
