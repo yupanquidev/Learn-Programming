b6b2050b5667be700013701ac6e18a72ed2ad684
Humberto Yupanqui Apaza <goypnq@gmail.com>
2023-04-06 18:57:10 -0500

    CRUD + Redux toolkit

---
+++ Programming-Languages/TypeScript/react/projects/03-crud/src/App.tsx
@@ -0,0 +1,16 @@
+import { Toaster } from 'sonner'
+import './App.css'
+import { CreateNewUser } from './components/CreateNewUser'
+import { ListOfUsers } from './components/ListOfUsers'
+
+function App() {
+	return (
+		<>
+			<ListOfUsers />
+			<CreateNewUser />
+			<Toaster richColors />
+		</>
+	)
+}
+
+export default App
