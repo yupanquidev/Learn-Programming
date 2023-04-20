b3ba6fcb0c8d4b608fe76150226a8cad8b02cb94
Humberto Yupanqui Apaza <goypnq@gmail.com>
2023-04-19 23:35:50 -0500

    Comments

---
+++ Programming-Languages/TypeScript/react/projects/05-comments/src/main.tsx
@@ -0,0 +1,12 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+)
