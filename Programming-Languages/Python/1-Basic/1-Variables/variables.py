8361bb9736632127b773e9e94801724d70ae4948
Humberto Yupanqui Apaza <goypnq@gmail.com>
2023-04-23 14:42:38 -0500

    variables

---
+++ Programming-Languages/Python/1-Basic/1-Variables/variables.py
@@ -0,0 +1,22 @@
+# Variables
+first_name = 'John'
+last_name = 'Doe'
+country = 'USA'
+age = 35
+weight = 70.5
+is_married = False
+is_true = True
+is_light_on = False
+
+# Declare multiple variables in one line
+first_name, last_name, country, age, weight, is_married = 'John', 'Doe', 'USA', 35, 70.5, False
+
+# Output
+print('First Name:', first_name)
+print('Last Name:', last_name)
+print('Country:', country)
+print('Age:', age)
+print('Weight:', weight)
+print('Is Married:', is_married)
+print('Is True:', is_true)
+print('Is Light On:', is_light_on)
