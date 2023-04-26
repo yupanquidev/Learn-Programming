5209e15c4407b1b22047811cf0d152d1a24f9dd9
Humberto Yupanqui Apaza <goypnq@gmail.com>
2023-04-26 18:26:31 -0500

    add zustand

---
+++ Programming-Languages/TypeScript/react/projects/06-quiz/src/store/questions.ts
@@ -0,0 +1,18 @@
+import { create } from 'zustand'
+import { Question } from '../types'
+
+interface State {
+  questions: Question[]
+  currentQuestion: number
+  fetchQuestions: (limit: number) => Promise<void>
+}
+
+export const useQuestionsStore = create<State>((set) => {
+  return {
+    questions: [],
+    currentQuestion: 0,
+    fetchQuestions: async (limit: number) => {
+      console.log('fetching questions')
+    }
+  }
+})
