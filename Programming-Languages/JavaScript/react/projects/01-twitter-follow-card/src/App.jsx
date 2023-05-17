96922c512c6484abfc96875c966548c8d36fd506
Humberto Yupanqui Apaza <goypnq@gmail.com>
2023-05-17 00:30:46 -0500

    fix: Twitter Follow Card

--- Programming-Languages/JavaScript/react/projects/01-twitter-follow-card/src/App.jsx
+++ Programming-Languages/JavaScript/react/projects/01-twitter-follow-card/src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
 
-function App() {
-  const [count, setCount] = useState(0)
+const users = [
+  {
+    userName: 'ypnqh',
+    name: 'Humberto Yupanqui Apaza',
+    isFollowing: false
+  },
+  {
+    userName: 'HustavoJhon',
+    name: 'Gustavo Jhon',
+    isFollowing: true
+  },
+  {
+    userName: 'Hokage-16',
+    name: 'Hokage-16',
+    isFollowing: false
+  }
+]
 
+export function App () {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>A quién seguir</h2>
+      <section className='App'>
+        {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+      </section>
+      <section>
+        <p>Mostrar más</p>
+      </section>
     </>
   )
 }
-
-export default App
