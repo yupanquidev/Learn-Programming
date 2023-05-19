2ae73d643cbaaf6bfcd4bd05f8f33b737c12452c
Humberto Yupanqui Apaza <goypnq@gmail.com>
2023-05-19 08:02:51 -0500

    fix file

--- JavaScript/react/projects/02-tic-tac-toe/vite.config.js
+++ JavaScript/react/projects/02-tic-tac-toe/vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8080
+  }
 })
