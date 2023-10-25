3f3c5b01e225b1f3cbffc3d31d4017b88cf6a9ab
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2023-10-25 18:54:25 -0500

    fix: indentation

--- Database/Exercises/1/practice.sql
+++ Database/Exercises/1/practice.sql
@@ -9,7 +9,7 @@ CREATE TABLE sneakers (
 );
 
 CREATE TABLE people_sneakers (
-   	id_people_sneakers SERIAL PRIMARY KEY,
+  id_people_sneakers SERIAL PRIMARY KEY,
 	id_people int,
 	id_sneakers int,
 	FOREIGN KEY (id_people) REFERENCES people(id_people),
