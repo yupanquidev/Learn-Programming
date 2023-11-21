c81d8c5e56e749ae425664e4fe831f4ccf87e431
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2023-11-20 19:39:56 -0500

    Add new files and update existing files

---
+++ JavaScript/vuejs/projects/01-x-follow-card/src/components/XFollowCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <article class="followCard">
+    <header class="header">
+      <img class="avatar" src="" alt="">
+      <div class="info">
+        <a href="#"></a>
+        <span class="info-username">@</span>
+      </div>
+    </header>
+    <button>
+      <span class="text"></span>
+      <span class="stop-follow">Dejar de seguir</span>
+    </button>
+  </article>
+</template>
