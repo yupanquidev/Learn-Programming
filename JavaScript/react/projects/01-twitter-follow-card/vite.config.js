3d2f36c391b1034f41b0bf652d5e879388c25e99
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2023-11-22 10:01:45 -0500

    Update Twitter Follow Card configuration

--- JavaScript/react/projects/01-twitter-follow-card/vite.config.js
+++ JavaScript/react/projects/01-twitter-follow-card/vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    strictPort: true,
-    port: 8080
-  }
+  plugins: [react()]
 })
