606dcaa5183a9b5e7c7ee78a5cd5f47f5708fbc7
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2023-12-29 12:10:54 -0500

    fix: foreign key constraints in tables.sql

--- Database/Odontology/tables.sql
+++ Database/Odontology/tables.sql
@@ -31,11 +31,7 @@ CREATE TABLE IF NOT EXISTS erp_odonto.erpo_docidentidad (
   fecha_nacimiento DATE NOT NULL,
   fecha_emision DATE NOT NULL,
   fecha_caducidad DATE NOT NULL,
-  direccion VARCHAR(30) NOT NULL,
-  id_provincia INT,
-  id_pais INT,
-  FOREIGN KEY (id_provincia) REFERENCES erpo_provincia(id),
-  FOREIGN KEY (id_pais) REFERENCES erpo_pais(id)
+  direccion VARCHAR(30) NOT NULL
 );
 
 -- Tabla de cliente
@@ -47,7 +43,12 @@ CREATE TABLE IF NOT EXISTS erp_odonto.erpo_cliente (
   telefono VARCHAR(15),
   email VARCHAR(255),
   id_docidentidad INT,
-  FOREIGN KEY (id_docidentidad) REFERENCES erpo_docidentidad(id)
+  id_pais INT,
+  id_provincia INT,
+  FOREIGN KEY (id_docidentidad) REFERENCES erpo_docidentidad(id),
+  FOREIGN KEY (id_pais) REFERENCES erpo_pais(id),
+  FOREIGN KEY (id_provincia) REFERENCES erpo_provincia(id)
+
 );
 
 -- Tabla de paciente
